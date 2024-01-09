5674a290a2805d2e73680a54d13c5aaabf914a41
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-09 15:43:00 -0500

    dev'd database.js logic handlers

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const id = await store.add(content);
+  console.log(`Added content with ID: ${id}`);
+  await tx.done;
+  console.error('putDb not implemented');
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = await store.getAll();
+  await tx.done;
+  return allContent;
+};
 initdb();
