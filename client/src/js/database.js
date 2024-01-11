c771f3692c4d24b6f1561f26e6808cc1d02a55db
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-10 19:02:13 -0500

    rebuilt to remove install button if already installed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -37,4 +37,5 @@ export const getDb = async () => {
     console.error('getDb not implemented');
   };
 };
+
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,15 @@
 const butInstall = document.getElementById('buttonInstall');
 let deferredPrompt; // Store event in the deferredPrompt variable for later use.
 
+if (localStorage.getItem('appInstalled')) {
+  butInstall.style.display = 'none';
+}
 // Logic for installing PWA
 window.addEventListener('beforeinstallprompt', (event) => {  
     event.preventDefault();
     deferredPrompt = event;
     butInstall.style.display = 'block';
 });
-
 
 // Click install button, call prompt() on the deferredPrompt to display install prompt. 
 butInstall.addEventListener('click', async () => {  
@@ -17,6 +19,7 @@ butInstall.addEventListener('click', async () => {
     // userChoice determines whether User accepted or dismissed install prompt. 
     if (choiceResult.outcome === 'accepted') {
       console.log('User accepted the install prompt');
+      localStorage.setItem('appInstalled', 'true');
     } else {
       console.log('User dismissed the install prompt');
     }
@@ -24,8 +27,11 @@ butInstall.addEventListener('click', async () => {
     butInstall.style.display = 'none';}
 });
 
-
 window.addEventListener('appinstalled', (event) => {
     console.log('App installed successfully'); // 'appinstalled' event logs message when app is installed.
+    localStorage.setItem('appInstalled', 'true');
 });
 
+if(window.matchMedia('(display-mode: standalone').matches) {
+  butInstall.style.display = 'none';
+};
