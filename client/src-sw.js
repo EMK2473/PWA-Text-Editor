d8f4900ff1d21a6f7f51aa88cb7aeccb2dd09786
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-14 14:33:02 -0500

    added more comments per grader request

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,3 +1,4 @@
+// Importing required modules from Workbox
 const { warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
@@ -5,39 +6,48 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Precaching and routing based on the provided manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Configuring CacheFirst strategy for pages
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
+    // CacheableResponsePlugin for handling certain response statuses
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
+    // ExpirationPlugin for setting a maximum age for cached items
     new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
 });
 
+// Warming up the page cache with specified URLs
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// Registering a route for navigation requests using the pageCache strategy
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-
+// Registering a route for image requests using CacheFirst strategy
 registerRoute(
   ({ request }) => request.destination === 'image',
   new CacheFirst({
     cacheName: 'asset-cache',
     plugins: [
+      // CacheableResponsePlugin for handling certain response statuses
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
+      // ExpirationPlugin for setting a maximum number of entries and maximum age for cached items
       new ExpirationPlugin({
         maxEntries: 60,
         maxAgeSeconds: 30 * 24 * 60 * 60, 
       })
     ],
-  }));
+  })
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,74 +1,96 @@
+// Importing necessary webpack plugins and modules
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+// Exporting webpack configuration as a function
 module.exports = () => {
   return {
+    // Setting development mode
     mode: 'development',
+    
+    // Entry points for the application
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    
+    // Output configuration
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: './index.html', 
-          title: "J.A.T.E."
-        }),
-        new InjectManifest({
-          swSrc: './src-sw.js', 
-          swDest: 'service-worker.js'
-        }),
-        new MiniCssExtractPlugin(),
-        //  workbox plugins for a service worker and manifest file
-        new WebpackPwaManifest({
-          name: 'Just Another Text Editor',
-          inject: true,
-          fingerprints: false,
-          short_name: 'J.A.T.E.',
-          description: 'A text editor that runs in the browser. Create notes or code snippets with or without an internet connection so you can reliably retrieve them for later use!',
-          start_url: './',
-          publicPath: './',
-          purpose: 'maskable',
-          background_color: '#225ca3',
-          theme_color: '#225ca3',
+    
+    // Webpack plugins configuration
+    plugins: [
+      // HTMLWebpackPlugin to generate HTML files
+      new HtmlWebpackPlugin({
+        template: './index.html', 
+        title: "J.A.T.E."
+      }),
+      
+      // Workbox plugin to inject a service worker
+      new InjectManifest({
+        swSrc: './src-sw.js', 
+        swDest: 'service-worker.js'
+      }),
+      
+      // MiniCssExtractPlugin to extract CSS into separate files
+      new MiniCssExtractPlugin(),
 
-          icons: [
-            {
-              src: path.resolve(__dirname, './src/images/logo.png'),
-              sizes: [96, 128, 192, 256, 384, 512],
-              destination: path.join('assets', 'icons'),
-            },
-          ],
-        }),
-      ],
-      // CSS loaders and babel to webpack.
-      module: {
-        rules: [
+      // WebpackPwaManifest for creating a web app manifest
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        inject: true,
+        fingerprints: false,
+        short_name: 'J.A.T.E.',
+        description: 'A text editor that runs in the browser. Create notes or code snippets with or without an internet connection so you can reliably retrieve them for later use!',
+        start_url: './',
+        publicPath: './',
+        purpose: 'maskable',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+
+        // Configuration for app icons
+        icons: [
           {
-            test: /\.css$/i,
-            use: [ MiniCssExtractPlugin.loader, 'css-loader'],
-          }, 
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
+            src: path.resolve(__dirname, './src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           },
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env'],
-              },
-            },
-          },
+        ],
+      }),
     ],
-  }
-  }
-}
+    
+    // Module configuration for loaders
+    module: {
+      rules: [
+        // CSS loader configuration
+        {
+          test: /\.css$/i,
+          use: [ MiniCssExtractPlugin.loader, 'css-loader'],
+        }, 
+        
+        // Asset loader configuration for images
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        
+        // Babel loader configuration for JavaScript
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    }
+  };
+};
